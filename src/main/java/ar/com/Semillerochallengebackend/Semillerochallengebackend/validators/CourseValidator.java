fc16d471695eb05f7f2601d492f534aad37cc5ec
MartinFiorde <fiorde_90@hotmail.com>
2023-07-13 01:16:21 -0300

    course individual view done

--- src/main/java/ar/com/Semillerochallengebackend/Semillerochallengebackend/validators/CourseValidator.java
+++ src/main/java/ar/com/Semillerochallengebackend/Semillerochallengebackend/validators/CourseValidator.java
@@ -1,22 +1,24 @@
 package ar.com.Semillerochallengebackend.Semillerochallengebackend.validators;
 
+import ar.com.Semillerochallengebackend.Semillerochallengebackend.enums.CourseStatusEnum;
 import ar.com.Semillerochallengebackend.Semillerochallengebackend.enums.CourseTurnEnum;
 import ar.com.Semillerochallengebackend.Semillerochallengebackend.errors.ServiceRuntimeException;
 import ar.com.Semillerochallengebackend.Semillerochallengebackend.models.Course;
 import ar.com.Semillerochallengebackend.Semillerochallengebackend.models.dto.CourseDTO;
 import ar.com.Semillerochallengebackend.Semillerochallengebackend.utils.StringUtils;
+import java.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CourseValidator {
-    
+
     public CourseDTO createValidator(CourseDTO dto) {
-//        validatePasswords(dto.getPassword(), passwordConfirm);
-//        validateEmail(dto.getEmail());
-//        validateRole(dto.getRole());
-//        validateGenericString(dto.getFirstName());
-//        validateGenericString(dto.getLastName());
-//        dto.setRegistrationDate(LocalDateTime.now());
+        validateGenericString(dto.getTitle());
+        validateCommission(dto.getCommission());
+        validateStartDate(dto.getStartDate());
+        validateStatus(dto.getStatus());
+        validateTurn(dto.getTurn());
+        validateGenericString(dto.getDescription());
         return dto;
     }
 
@@ -32,7 +34,18 @@ public class CourseValidator {
         return dto;
     }
 
-    public String validateRole(String role) throws ServiceRuntimeException {
+    public String validateStatus(String status) throws ServiceRuntimeException {
+        validateGenericString(status);
+        CourseStatusEnum[] courseStatusEnums = CourseStatusEnum.values();
+        for (CourseStatusEnum courseStatusEnum : courseStatusEnums) {
+            if (courseStatusEnum.toString().equals(status)) {
+                return status;
+            }
+        }
+        throw new ServiceRuntimeException("El turno ingresado no es válido.");
+    }
+
+    public String validateTurn(String role) throws ServiceRuntimeException {
         validateGenericString(role);
         CourseTurnEnum[] courseTurns = CourseTurnEnum.values();
         for (CourseTurnEnum courseTurn : courseTurns) {
@@ -60,11 +73,26 @@ public class CourseValidator {
         return string;
     }
 
+    public Integer validateCommission(Integer number) throws ServiceRuntimeException {
+        if (StringUtils.nullOrEmpty(number.toString())) {
+            throw new ServiceRuntimeException("No puede haber campos vacios.");
+        }
+        if (number <= 0) {
+            throw new ServiceRuntimeException("Debe ingresar un numero de comisión válido.");
+        }
+        return number;
+    }
+
+    public LocalDate validateStartDate(LocalDate date) throws ServiceRuntimeException {
+        //TODO
+        return date;
+    }
+
     public Course validateActiveStatus(Course course) throws ServiceRuntimeException {
         if (!course.isActive()) {
             throw new ServiceRuntimeException("El usuario solicitado se encuentra deshabilitado.");
         }
         return course;
     }
-    
+
 }
