b7b090b00c6db77b51fe418fe9dff62b57a163fd
fiorde90 <fiorde_90@hotmail.com>
2024-08-06 18:16:43 -0300

    test improvements

--- src/test/java/ar/com/Semillerochallengebackend/Semillerochallengebackend/models/converters/CourseConverterTest.java
+++ src/test/java/ar/com/Semillerochallengebackend/Semillerochallengebackend/models/converters/CourseConverterTest.java
@@ -5,7 +5,8 @@ import ar.com.Semillerochallengebackend.Semillerochallengebackend.models.dto.Cou
 import ar.com.Semillerochallengebackend.Semillerochallengebackend.constants.CourseStatusEnum;
 import ar.com.Semillerochallengebackend.Semillerochallengebackend.constants.CourseTurnEnum;
 import java.time.LocalDate;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,14 +16,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 // LSITA DE VIDEOS DE REFERENCIA (VIDEO 1 PARA REPOSITORIO. VIDEO 2 PARA SERVICIOS (APLICA ACA CON CIERTAS ADAPTACIONES)
 // https://www.youtube.com/watch?v=tG-TwkIhjOk
 @ExtendWith(MockitoExtension.class)
-public class CourseConverterTest {
+class CourseConverterTest {
 
     @InjectMocks
     private CourseConverter courseConverter;
     
     @Test
     @DisplayName("It should turn CourseDTO into a valid Course Entity")
-    public void testDtoToEntity() {
+    void testDtoToEntity() {
         // ARRANGE - Setting up the data that required for the test case
         Course expectedResult = new Course(null, true, "Java Inicial", 1, LocalDate.parse("2023-07-30"), CourseStatusEnum.PROGRAMED, CourseTurnEnum.MORNING, "curso inicial de java", null, null);
         CourseDTO dto = new CourseDTO(null, true, "Java Inicial", 1, LocalDate.parse("2023-07-30"), "PROGRAMED", "MORNING", "curso inicial de java", null, null);
@@ -31,13 +32,13 @@ public class CourseConverterTest {
         Course result = courseConverter.dtoToEntity(dto);
 
         // ASSERT - Verify that the expected result is correct or not
-        assertEquals(expectedResult, result);
-        assertNotNull(result);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(expectedResult, result);
     }
     
     @Test
     @DisplayName("It should turn Course entity into a valid CourseDTO")
-    public void testEntityToDto() {
+    void testEntityToDto() {
         // ARRANGE - Setting up the data that required for the test case
         CourseDTO expectedResult = new CourseDTO(null, true, "Java Inicial", 1, LocalDate.parse("2023-07-30"), "PROGRAMED", "MORNING", "curso inicial de java", null, null);
         Course entity = new Course(null, true, "Java Inicial", 1, LocalDate.parse("2023-07-30"), CourseStatusEnum.PROGRAMED, CourseTurnEnum.MORNING, "curso inicial de java", null, null);
@@ -46,7 +47,7 @@ public class CourseConverterTest {
         CourseDTO result = courseConverter.entityToDto(entity);
 
         // ASSERT - Verify that the expected result is correct or not
-        assertEquals(expectedResult, result);
-        assertNotNull(result);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(expectedResult, result);
     }
 }
